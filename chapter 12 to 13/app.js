45137cc71a7b0c76baf63ccded4935ae5936cadc
mziyan-dev <m.ziyan@paz.agency>
2025-11-11 18:53:11 +0500

    chapter 12 to 13

---
+++ chapter 12 to 13/app.js
@@ -0,0 +1,85 @@
+
+// ----------------------------------chapter 12 to 13 - -----------------------------------------------------// 
+
+
+// var char = prompt("Enter a single character");
+
+// var code = char.charCodeAt(0);
+
+// // 3. Check karo type
+// if (code >= 48 && code <= 57) {
+//     alert("You entered a number");
+// } 
+// else if (code >= 65 && code <= 90) {
+//     alert("You entered an uppercase letter");
+// } 
+// else if (code >= 97 && code <= 122) {
+//     alert("You entered a lowercase letter");
+// } 
+// else {
+//     alert("You entered a special character or symbol");
+// }
+
+
+
+// var num1  = Number(prompt("enter 1st number"))
+// var num2  = Number(prompt("enter 2nd number"));
+
+// if (num1 > num2) {
+//     alert("your first number greater then second number")
+// }
+// else if (num2 > num1) {
+//     alert("your second number greater then first number")
+// }
+// else if (num1 === num2) {
+//     alert("your number are equel")
+// }
+
+
+
+// var num =Number(prompt("enter a number"));
+
+// if (num > 1) {
+//     alert("positive number")
+// }
+// else if (num < 1) {
+//     alert("nagative number")
+// }
+// else if (num === 1) {
+//     alert("Equel Number")
+// }
+
+
+// var char = prompt("Enter a Character")
+// char = char.toLowerCase();
+
+// if (char === "a" || char === "e" || char === "i" || char === "o"
+//     || char === "u"
+// ) {
+//     alert("Vowel character")
+// }else{
+//     alert("not a Vowel")
+// }
+
+
+
+// var passWord = 1234567
+// var userInput = Number(prompt("enter a Password"))
+
+// if (passWord === userInput) {
+//     alert("correct password")
+// }
+// else if (userInput === "") {
+//     alert("Please enter a password")
+// }
+// else if (passWord !== userInput) {
+//     alert("Incorrect password")
+// }
+
+// var time =Number(prompt("enter a hour"))
+// var hour = 18
+// if (time < hour) {
+//     alert("Good morning")
+// } else if (time > hour) {
+//     alert("Good evening")
+// }

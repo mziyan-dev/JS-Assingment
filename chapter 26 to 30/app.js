7c6661f489352eb3746ad6c683791c7932b62835
mziyan-dev <m.ziyan@paz.agency>
2025-11-13 18:29:57 +0500

    chapter 26 to 30

---
+++ chapter 26 to 30/app.js
@@ -0,0 +1,95 @@
+// ---------------------------------------Chapter 26 to 30-----------------------------------------------
+
+
+
+
+
+
+// var userInput = prompt("Enter a positive integer number:");
+
+
+// var userNum = parseFloat(userInput);
+// var roundedNumber = Math.round(userInput);
+// var flooredNumber = Math.floor(userInput);
+// var ceiledNumber = Math.ceil(userInput);
+
+
+// console.log("orignal number: " + userNum);
+// console.log("Number after round: " + roundedNumber);
+// console.log("flored number: " + flooredNumber);
+// console.log("Ciel Number: " +ceiledNumber )
+
+
+
+// var userInput = prompt("Enter a negative integer number:");
+
+
+// var userNum = parseFloat(userInput);
+// var roundedNumber = Math.round(userInput);
+// var flooredNumber = Math.floor(userInput);
+// var ceiledNumber = Math.ceil(userInput);
+
+
+// console.log("orignal number: " + userNum);
+// console.log("Number after round: " + roundedNumber);
+// console.log("flored number: " + flooredNumber);
+// console.log("Ciel Number: " +ceiledNumber )
+
+
+
+// var userInput = prompt("Enter a positive/negative integer number:")
+
+
+// var absoluteValue = Math.abs(userInput);
+
+// console.log("The orignal value is " + userInput);
+// console.log("The absolute value is " + absoluteValue);
+
+
+// var randomNumber = Math.random()*6;
+// var diceNumber = Math.ceil(randomNumber);
+
+// console.log("your random diecs number is: "+ diceNumber);
+
+
+// var randomNumber = Math.random()*2;
+// var diceNumber = Math.floor(randomNumber);
+
+
+// if (diceNumber === 0) {
+//     console.log("your Random coin is: heads" );
+    
+// }if (diceNumber === 1) {
+//     console.log("your Random coin is: tails");
+// }
+
+
+
+// var randomNumber = Math.random()*100;
+// var diceNumber = Math.floor(randomNumber);
+
+// console.log("The random number between 1 and 100 is: " + diceNumber);
+
+
+
+
+// var userWeight = prompt("Enter your weight in kilograms:");
+// var weightInKg = parseFloat(userWeight);
+
+// console.log("The weight of user is: " + weightInKg + " kilograms.");
+
+
+
+
+// var secretNumber = Math.floor(Math.random() * 5) + 1;
+// var userGuess = parseInt(prompt("Guess the secret number between 1 and 10:"));
+
+// if (userGuess === secretNumber ) {
+//     alert("Congratulations! You guessed the secret number " + secretNumber + " correctly.");
+// }else{
+//     alert("Sorry, the secret number was " + secretNumber + ". Better luck next time!");
+// }
+
+
+
+// ---------------------------------------Chapter 26 to 30-----------------------------------------------
